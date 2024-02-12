7d7d2a7072120778f6340e1ac6771d8296e94f0a
Baivab Mukherjee <shiladityamukherjee5486@gmail.com>
2024-02-12 10:48:03 +0530

    Minor changes

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -3,6 +3,7 @@ import './css/AdminLogin.css';
 import { loginUser, logoutUser } from '../service/api1'; // Assuming loginUser function is correctly implemented in api1.js
 import SearchJob from './Jobi'; // Assuming SearchJob component is correctly implemented
 import { NavLink } from 'react-router-dom';
+import SignIn from '../googleSignin/signin';
 
 const UserLogin = () => {
   const [username, setUsername] = useState('');
@@ -26,9 +27,13 @@ const UserLogin = () => {
   };
 
   const handleLogout = async () => {
+    localStorage.clear();
+    window.location.reload();
     try {
       await logoutUser();
       setIsLoggedIn(false);
+      
+
     } catch (error) {
       console.error('Error:', error);
       setError('An error occurred. Please try again.');
@@ -38,42 +43,41 @@ const UserLogin = () => {
   if (isLoggedIn) {
     return (
       <div>
+        <button style={{ marginLeft: '1400px', paddingTop: '-40px', display: 'block', backgroundColor: 'white', color: 'wheat' }} onClick={handleLogout}>Logout</button>
         <SearchJob />
-        <button style={{marginLeft:'1400px',paddigTop:'-40px',display:'block',backgroundColor:'white',color:'wheat'}} onClick={handleLogout}>Logout</button>
-        
       </div>
     );
   }
 
   return (
-    <>
-    <div className='ll' style={{backgroundColor:'aqua',height:'800px',width:'100%'}}>
-    <div className="login-container">
-      <h2 className="login-header">
-      User Login</h2>
-      <div className="input-container">
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+    <div className='ll' style={{ backgroundColor: 'aqua', height: '800px', width: '100%' }}>
+      <div className="login-container">
+        <h2 className="login-header">
+          USER LOGIN
+        </h2>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="login-button" onClick={handleLogin}>Login</button>
+        {error && <div>{error}</div>}
+        Don't have an account?
+        <NavLink to="/Form1">create one</NavLink>
+        <SignIn />
       </div>
-      <div className="input-container">
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      </div>
-      <button className="login-button" onClick={handleLogin}>Login</button>
-      {error && <div>{error}</div>}
-      Don't have an account?
-      <NavLink to="/Form1">create one</NavLink>
     </div>
-    </div>
-    </>
   );
 };
 
